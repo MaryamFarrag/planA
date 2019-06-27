61a4857cc25c58ba89d6167844497c62c9400ab5
unknown <maryamfarrag0@gmail.com>
2019-06-27 23:38:01 +0200

    secUpdate

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/planaa'));
+app.use(express.static(__dirname + '/dist/plan-b'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/planaa/index.html'));
+res.sendFile(path.join(__dirname+'/dist/plan-b/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
